dee3c98fda481900e95ba42faa934bd7b2db840f
leon60740 <leon60740@example.com>
2024-03-15 15:27:25 +0800

    refactor: extract sortedMemberProperties variable to Utils.kt

--- jsonlib/src/main/java/com/example/leonapplication/jsonlib/KsonSerialize.kt
+++ jsonlib/src/main/java/com/example/leonapplication/jsonlib/KsonSerialize.kt
@@ -2,7 +2,6 @@ package com.example.leonapplication.jsonlib
 
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty1
-import kotlin.reflect.full.memberProperties
 
 fun serialize(obj: Any): String = buildString { serializeObject(obj) }
 
@@ -16,23 +15,21 @@ private const val NULL_VAL = "null"
 
 private val classCache = hashMapOf<KClass<*>, Collection<KProperty1<Any, *>>>()
 
-private val <T : Any> KClass<T>.declaredMemberProperties: Collection<KProperty1<T, *>>
-  get() = java.declaredFields.withIndex()
-    .associate { it.value.name to it.index }
-    .let { orderById ->
-      memberProperties.sortedBy { orderById[it.name] }
-    }
-
 private fun StringBuilder.serializeObject(obj: Any) {
   if (obj is List<*>) {
     serializeList(obj)
     return
   }
   val kClass = obj.javaClass.kotlin
-  val members = classCache[kClass]
-    ?: kClass.declaredMemberProperties.also { classCache[kClass] = it }
-
-  members.joinToStringBuilder(this, separator = SEPARATOR, prefix = OBJECT_START, postfix = OBJECT_ENT) { prop ->
+  val members = classCache.getOrPut(kClass) {
+    kClass.sortedMemberProperties
+  }
+  members.joinToStringBuilder(
+    this,
+    separator = SEPARATOR,
+    prefix = OBJECT_START,
+    postfix = OBJECT_ENT
+  ) { prop ->
     serializeString(prop.name)
     append(INDICATOR)
     serializePropertyValue(prop.call(obj))
--- jsonlib/src/main/java/com/example/leonapplication/jsonlib/Utils.kt
+++ jsonlib/src/main/java/com/example/leonapplication/jsonlib/Utils.kt
@@ -1,5 +1,9 @@
 package com.example.leonapplication.jsonlib
 
+import kotlin.reflect.KClass
+import kotlin.reflect.KProperty1
+import kotlin.reflect.full.memberProperties
+
 fun <T> Iterable<T>.joinToStringBuilder(
   stringBuilder: StringBuilder,
   separator: CharSequence = ", ",
@@ -15,3 +19,14 @@ fun <T> Iterable<T>.joinToStringBuilder(
     ""
   }
 }
+
+
+/**
+ * get memberProperties of kClass with its declared order
+ */
+internal val <T : Any> KClass<T>.sortedMemberProperties: Collection<KProperty1<T, *>>
+  get() = java.declaredFields.withIndex()
+    .associate { it.value.name to it.index }
+    .let { orderById ->
+      memberProperties.sortedBy { orderById[it.name] }
+    }
