ae3da406055a3f2f419f933caa6872577a2ce10e
leon60740 <leon60740@example.com>
2024-03-27 11:43:40 +0800

    feat: complet download server range support

--- downloadlib/server-app/index.js
+++ downloadlib/server-app/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const range = require('express-range')
+const fs = require('fs')
 
 const port = 8989
 const app = express()
@@ -25,12 +26,41 @@ app.get('/', (req, res) => {
 app.get('/download/:file', (req, res) => {
   const file = req.params.file;
   const filePath = path.join(dir, file)
-  res.download(filePath, (err) => {
-    if (err) {
-      console.error(`download error: ${err.message}`)
-      res.status(404).send('file not found')
+
+  const fileSize = fs.statSync(filePath).size;
+  console.log(`file size: ${fileSize}`)
+
+  // Get the Range header value
+  const rangeHeader = req.headers.range;
+  console.log(`range header: ${rangeHeader}`)
+
+  if (rangeHeader) {
+    try {
+      const [partialstart, partialend] = rangeHeader.replace(/bytes=/, "").split("-");
+
+      const start = parseInt(partialstart, 10);
+      const end = partialend ? parseInt(partialend, 10) : fileSize;
+      const chunksize = end - start;
+
+      res.range({
+        first: start,
+        end: end,
+        length: chunksize
+      })
+      res.sendFile(filePath, { start, end });
+    } catch (e) {
+      console.error(`range error: ${e.message}`)
+      // Invalid Range header
+      res.status(416).send('Range Not Satisfiable');
     }
-  })
+  } else {
+    res.download(filePath, (err) => {
+      if (err) {
+        console.error(`download error: ${err.message}`)
+        res.status(404).send('file not found')
+      }
+    })
+  }
 })
 
 app.listen(port)
