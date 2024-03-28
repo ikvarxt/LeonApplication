8f522c3219bd643448114ceb3cbb17819472be1c
leon60740 <leon60740@example.com>
2024-03-28 09:41:55 +0800

    test: optimize partial function test

--- app/src/test/java/com/example/leonapplication/app/partialfunc/ChainPatternTest.kt
+++ app/src/test/java/com/example/leonapplication/app/partialfunc/ChainPatternTest.kt
@@ -5,41 +5,28 @@ package com.example.leonapplication.app.partialfunc
 import org.junit.Assert.*
 import org.junit.Test
 
-typealias ApplyCheck = (ApplyEvent) -> Boolean
-typealias ApplyResult = (ApplyEvent) -> Handler?
-
-sealed class Handler {
-  data object Student : Handler()
-  data object Teacher : Handler()
-  data object President : Handler()
-  data object Reject : Handler()
-}
 
 class ChainPattern {
 
-  private val student = {
-    val definedAt: ApplyCheck = { it.cost < 500 }
-    val handler: ApplyResult = { Handler.Student }
-    PartialFunction(definedAt, handler)
-  }()
+  private val student = ApplyFunc(
+    { it.cost < 500 },
+    { Handler.Student }
+  )
 
-  private val teacher = {
-    val definedAt: ApplyCheck = { it.cost < 10000 }
-    val handler: ApplyResult = { Handler.Teacher }
-    PartialFunction(definedAt, handler)
-  }()
+  private val teacher = ApplyFunc(
+    { it.cost < 10000 },
+    { Handler.Teacher }
+  )
 
-  private val president = {
-    val definedAt: ApplyCheck = { it.cost < 100_000 }
-    val handler: ApplyResult = { Handler.President }
-    PartialFunction(definedAt, handler)
-  }()
+  private val president = ApplyFunc(
+    { it.cost < 100_000 },
+    { Handler.President }
+  )
 
-  private val reject = {
-    val d: ApplyCheck = { it.cost < Int.MAX_VALUE }
-    val h: ApplyResult = { Handler.Reject }
-    PartialFunction(d, h)
-  }()
+  private val reject = ApplyFunc(
+    { true },
+    { Handler.Reject }
+  )
 
   val applyChain = student orElse teacher orElse president orElse reject
 
@@ -63,10 +50,19 @@ class ChainPattern {
     val event = ApplyEvent("new building", 1_000_000)
     assertEquals(applyChain(event), Handler.Reject)
   }
-
 }
 
-class PartialFunction<in P1, out R>(private val definetAt: (P1) -> Boolean, private val f: (P1) -> R) : (P1) -> R {
+typealias ApplyCheck = (ApplyEvent) -> Boolean
+typealias ApplyResult = (ApplyEvent) -> Handler
+
+sealed class Handler {
+  data object Student : Handler()
+  data object Teacher : Handler()
+  data object President : Handler()
+  data object Reject : Handler()
+}
+
+open class PartialFunction<in P1, out R>(private val definetAt: (P1) -> Boolean, private val f: (P1) -> R) : (P1) -> R {
   override fun invoke(p1: P1): R {
     if (definetAt(p1)) {
       return f(p1)
@@ -86,3 +82,5 @@ infix fun <P1, R> PartialFunction<P1, R>.orElse(that: PartialFunction<P1, R>): P
     }
   }
 }
+
+class ApplyFunc(d: ApplyCheck, h: ApplyResult) : PartialFunction<ApplyEvent, Handler>(d, h)
