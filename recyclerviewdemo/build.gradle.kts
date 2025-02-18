5790538e94f8296166e8bc7f95d4ef307990d851
ikvarxt <leonekola@outlook.com>
2025-02-18 19:33:04 +0800

    fix: [rv demo] build.gradle kotlin plugin error

--- recyclerviewdemo/build.gradle.kts
+++ recyclerviewdemo/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
   alias(libs.plugins.android.library)
-  alias(libs.plugins.android.kotlin)
+  alias(libs.plugins.kotlin.android)
 }
 
 android {
@@ -22,10 +22,6 @@ android {
         "proguard-rules.pro"
       )
     }
-  }
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
   }
   kotlinOptions {
     jvmTarget = "1.8"
